e63d36656b6a2c13907fe7c3a7f692e5e56be451
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-08 15:02:03 +0900

    Add cocktails and doses to make new recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, except: [:edit, :update] do
+    resources :doses, only: [:index, :new, :create, :destroy]
+  end
+  # list all cocktails
+  # /cocktails
+  # show individual cocktail -> cocktail should contain doses of ingredients
+  # /cocktails/:id
+  # to make new, only need a name (no doses)
+  # /cocktails/new
+  # to "edit" cocktail, add dose of ingredient to it
+  resources :ingredients, only: [:index, :show] # no create, no update
+
+  # resources :doses
+  # add dose to cocktail
+  # /cocktails/:id/doses/new
+  # delete dose from cocktail
+  # /cocktails/:id/doses
+
 end
